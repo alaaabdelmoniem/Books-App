d78494968737c3576fc576ea306b72849fb3b617
Alaa <Alaaabdelmoniem5@gmail.com>
2025-10-24 02:41:26 +0300

    refactor init state & Navigation to home page

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:books_app/constants.dart';
 import 'package:books_app/core/utils/assets.dart';
+import 'package:books_app/features/home/presentation/views/home_view.dart';
 import 'package:books_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,11 +20,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-
     initSlidingAnimation();
+    navigationToHome();
   }
-
-  
 
   @override
   void dispose() {
@@ -41,19 +43,28 @@ class _SplashViewBodyState extends State<SplashViewBody>
     );
   }
 
-
-
   void initSlidingAnimation() {
-     animationController = AnimationController(
+    animationController = AnimationController(
       vsync: this,
       duration: Duration(seconds: 1),
     );
-    
+
     slidingAnimation = Tween<Offset>(
       begin: const Offset(0, 2),
       end: Offset.zero,
     ).animate(animationController);
     animationController.forward();
   }
-}
 
+  void navigationToHome() {
+
+    Future.delayed(Duration(seconds: 2), () {
+      Get.to(
+        () => HomeView(),
+        transition: Transition.fade,
+      
+        duration: kTransitionDuration,
+      );
+    });
+  }
+}
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff100B20);
 
+const kTransitionDuration = Duration(milliseconds: 700);
