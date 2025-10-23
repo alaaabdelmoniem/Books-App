81c5eac87ec503f91e994748ecc2e0f530f95257
Alaa <Alaaabdelmoniem5@gmail.com>
2025-10-23 20:46:39 +0300

    splash view ui done

--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,4 @@
-import 'package:books_app/Features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:books_app/features/splash/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -7,6 +7,7 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      
       body: SplashViewBody(),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
-import 'package:books_app/Features/splash/presentation/views/splash_view.dart';
+import 'package:books_app/constants.dart';
+import 'package:books_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
-  runApp(const Books());
+  runApp(const BooksApp());
 }
 
-class Books extends StatelessWidget {
-  const Books({super.key});
+class BooksApp extends StatelessWidget {
+  const BooksApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      // navigation
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
       debugShowCheckedModeBanner: false,
+
       home: SplashView(),
     );
   }
