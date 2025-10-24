3e7b0a1a1b5a718d23be1d3b9d676a9927226069
Alaa <Alaaabdelmoniem5@gmail.com>
2025-10-24 20:40:33 +0300

    create custom book item

---
+++ lib/Features/home/presentation/views/widgets/custom_item_book.dart
@@ -0,0 +1,27 @@
+import 'package:books_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomBookItem extends StatelessWidget {
+  const CustomBookItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: AspectRatio(
+        aspectRatio: 2.8 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:books_app/features/home/presentation/views/widgets/custom_item_book.dart';
 import 'package:flutter/material.dart';
 import 'package:books_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 
@@ -6,6 +7,6 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [CustomAppBar()]);
+    return const Column(children: [CustomAppBar(), CustomBookItem()]);
   }
 }
