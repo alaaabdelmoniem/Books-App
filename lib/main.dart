2e2d8df24e9bfc2e2c2287de36cc7af5945a4667
Alaa <Alaaabdelmoniem5@gmail.com>
2025-10-26 16:59:19 +0300

    add font family to our app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:books_app/constants.dart';
 import 'package:books_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooksApp());
@@ -14,7 +15,10 @@ class BooksApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       // navigation
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
       debugShowCheckedModeBanner: false,
 
       home: const SplashView(),
