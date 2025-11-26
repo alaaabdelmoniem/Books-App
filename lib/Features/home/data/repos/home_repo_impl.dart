9275129f041e36728a06e9e00430c5340318bbc1
Alaa <Alaaabdelmoniem5@gmail.com>
2025-11-26 20:49:09 +0200

    create api service

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:books_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:books_app/Features/home/data/repos/home_repo.dart';
+import 'package:books_app/core/utils/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRpoImplemention implements HomeReop{
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
