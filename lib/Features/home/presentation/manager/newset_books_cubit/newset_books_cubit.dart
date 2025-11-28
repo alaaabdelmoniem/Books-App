0b727c7450129e22a3d1a6751ce6b9761ff1d220
Alaa <Alaaabdelmoniem5@gmail.com>
2025-11-28 22:21:06 +0200

    create newset books cubit method

--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,10 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:books_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:books_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewestBooksInitial());
-  
+  NewsetBooksCubit(this.homeReop) : super(NewestBooksInitial());
+  final HomeReop homeReop;
+  Future<void> fetchNewstBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeReop.fetchNewsetBooks();
+    result.fold(
+      (failure) {
+        emit(NewsetBooksFailure(errorMessage: failure.errorMessage));
+      },
+      (books) {
+        emit(NewsetBooksSuccess(books: books));
+      },
+    );
+  }
 }
